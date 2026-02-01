36984807f49741b703cb181ea51fc2a197f9de55
Godson Oyibayado <godsonoyibayado@gmail.com>
2026-02-01 16:32:51 +0100

    Updated portfoflio

--- portfolio.js
+++ portfolio.js
@@ -1,14 +1,4 @@
 "use strict"
-document.addEventListener("DOMContentLoaded", ()=>{
-    const slides =document.querySelectorAll(".slide");
-    let index = 0;
-    setInterval(()=>{
-        slides[index].classList.remove("active");
-        index = (index + 1) % slides.length;
-        slides[index].classList.add("active");
-    }, 2500);
-});
-
 const cards = document.querySelectorAll(".services");
 const kevwe = new IntersectionObserver((entries) =>{
     entries.forEach((entry) =>{
